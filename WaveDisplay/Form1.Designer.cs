c43bd2eb9e9a30cd417214809af12a9f28140218
fantasy9876 <uoygnivol@gmail.com>
2013-05-03 23:22:45 -0700

    ! add new repo

---
+++ WaveDisplay/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WaveDisplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.levelScrollBar = new System.Windows.Forms.HScrollBar();
+            this.zoomIn = new System.Windows.Forms.Button();
+            this.zoomOut = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1276, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1276, 758);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // levelScrollBar
+            // 
+            this.levelScrollBar.LargeChange = 1;
+            this.levelScrollBar.Location = new System.Drawing.Point(99, 27);
+            this.levelScrollBar.Maximum = 0;
+            this.levelScrollBar.Name = "levelScrollBar";
+            this.levelScrollBar.Size = new System.Drawing.Size(1161, 39);
+            this.levelScrollBar.TabIndex = 2;
+            this.levelScrollBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.levelScrollBar_Scroll);
+            // 
+            // zoomIn
+            // 
+            this.zoomIn.Location = new System.Drawing.Point(12, 27);
+            this.zoomIn.Name = "zoomIn";
+            this.zoomIn.Size = new System.Drawing.Size(34, 39);
+            this.zoomIn.TabIndex = 3;
+            this.zoomIn.Text = "+";
+            this.zoomIn.UseVisualStyleBackColor = true;
+            this.zoomIn.Click += new System.EventHandler(this.zoomIn_Click);
+            // 
+            // zoomOut
+            // 
+            this.zoomOut.Location = new System.Drawing.Point(52, 27);
+            this.zoomOut.Name = "zoomOut";
+            this.zoomOut.Size = new System.Drawing.Size(34, 39);
+            this.zoomOut.TabIndex = 4;
+            this.zoomOut.Text = "-";
+            this.zoomOut.UseVisualStyleBackColor = true;
+            this.zoomOut.Click += new System.EventHandler(this.zoomOut_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1276, 782);
+            this.Controls.Add(this.zoomOut);
+            this.Controls.Add(this.zoomIn);
+            this.Controls.Add(this.levelScrollBar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.HScrollBar levelScrollBar;
+        private System.Windows.Forms.Button zoomIn;
+        private System.Windows.Forms.Button zoomOut;
+    }
+}
+
---
+++ WaveDisplay/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WaveDisplay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        public static WaveIn waveIn = new WaveIn();
+        public static WaveIn waveZoom;
+        public static int currentLevel;
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            waveIn.waveExtract("d.wav");
+            waveIn.DrawAudio(waveIn.leftData, pictureBox1);
+            waveZoom = new WaveIn(waveIn);
+            currentLevel = waveZoom.leftData.Count;
+            Console.WriteLine("currentLevel : " + currentLevel.ToString());
+            levelScrollBar.Maximum = 0;
+
+        }
+
+       
+
+        private void zoomIn_Click(object sender, EventArgs e)
+        {
+            int tempLevel = currentLevel;
+            int startLevel;
+            if (currentLevel > 20000)
+                currentLevel -= 10000;
+            else if (currentLevel <= 20000 && currentLevel > 1000)
+                currentLevel -= 500;
+            else if (currentLevel <= 1000 && currentLevel > 500)
+                currentLevel -= 100;
+            else
+                currentLevel -= 50;
+            Console.WriteLine("currentLevel : " + currentLevel.ToString());           
+            if (currentLevel > 0 )
+            {
+                startLevel = levelScrollBar.Value;
+                if (waveIn.leftData.Count - startLevel < currentLevel)
+                    startLevel = waveIn.leftData.Count - currentLevel;
+                waveZoom.leftData = waveIn.leftData.GetRange(startLevel, currentLevel);
+                waveZoom.DrawAudio(waveZoom.leftData, pictureBox1);
+                levelScrollBar.Maximum = waveIn.leftData.Count -currentLevel;
+            }
+            else
+            {
+                currentLevel = tempLevel;
+                MessageBox.Show("Can't zoom in!");
+            }
+        }
+
+        private void zoomOut_Click(object sender, EventArgs e)
+        {
+            int startLevel;
+
+            if (currentLevel < 500)
+                currentLevel += 50;
+            else if (currentLevel >= 500 && currentLevel < 1000)
+                currentLevel += 100;
+            else if (currentLevel >= 1000 && currentLevel < 20000)
+                currentLevel += 500;
+            else
+                currentLevel += 10000;
+            Console.WriteLine("currentLevel : " + currentLevel.ToString());
+            if (currentLevel < waveIn.leftData.Count)
+            {
+                startLevel = levelScrollBar.Value;
+                if (waveIn.leftData.Count - startLevel < currentLevel)
+                    startLevel = waveIn.leftData.Count - currentLevel;
+                waveZoom.leftData = waveIn.leftData.GetRange(startLevel, currentLevel);
+                waveZoom.DrawAudio(waveZoom.leftData, pictureBox1);
+                levelScrollBar.Maximum = waveIn.leftData.Count - currentLevel;
+            }
+            else
+            {
+                currentLevel = waveIn.leftData.Count;
+                MessageBox.Show("Can't zoom out!");
+            }
+               
+            
+        }
+
+        private void levelScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {         
+            waveZoom.leftData = waveIn.leftData.GetRange(levelScrollBar.Value, currentLevel);
+            waveZoom.DrawAudio(waveZoom.leftData, pictureBox1);
+            Console.WriteLine("value : " + levelScrollBar.Value.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class WaveIn
+    {
+        struct waveHeader
+        {
+            public byte[] riffID;
+            public byte[] fileFormat;
+            public byte[] fmtID;
+            public UInt16 audioFormat;
+            public UInt16 nChannels;
+            public uint sampleRate;
+            public uint byteRate;
+            public UInt16 blockAlign;
+            public UInt16 bitSample;
+            public byte[] dataID;
+            public uint dataSize;
+        }
+        public List<short> leftData = new List<short>(); //Aussme if filewave is mono, use this leftData to extract data
+        public List<short> rightData= new List<short>();
+
+        public WaveIn(WaveIn previousWave)
+        {
+            leftData = previousWave.leftData;
+            rightData = previousWave.rightData;
+        }
+        public WaveIn()
+        {
+        }
+
+        public void waveExtract(string filename)
+        {
+            byte[] tmpByte;
+            waveHeader wavHeader;
+
+            try 
+            {
+                using (FileStream wave_fs = new FileStream(filename, FileMode.Open, FileAccess.Read))
+                {
+                    using (BinaryReader br = new BinaryReader(wave_fs))
+                    {
+
+                        wavHeader.riffID = br.ReadBytes(4);
+                        tmpByte = br.ReadBytes(4);
+                        wavHeader.fileFormat = br.ReadBytes(4);
+                        wavHeader.fmtID = br.ReadBytes(4);
+                        tmpByte = br.ReadBytes(4);
+                        wavHeader.audioFormat = br.ReadUInt16();
+                        wavHeader.nChannels = br.ReadUInt16();
+                        wavHeader.sampleRate = br.ReadUInt32();
+                        wavHeader.byteRate = br.ReadUInt32();
+                        wavHeader.blockAlign = br.ReadUInt16();
+                        wavHeader.bitSample = br.ReadUInt16();
+                        wavHeader.dataID = br.ReadBytes(4);
+                        wavHeader.dataSize = br.ReadUInt32();
+
+                        //assume the file read in has 16 bit per sample
+                        for (int i = 0; i < wavHeader.dataSize / wavHeader.blockAlign; i++)
+                        {
+                            if (wavHeader.nChannels == 1)
+                                leftData.Add(br.ReadInt16());
+                            else if (wavHeader.nChannels == 2)
+                            {
+                                leftData.Add(br.ReadInt16());
+                                rightData.Add(br.ReadInt16());
+                            }
+                        }
+
+                    }
+                    string riff = Encoding.UTF8.GetString(wavHeader.riffID);
+                    string format =Encoding.UTF8.GetString(wavHeader.fileFormat);
+                    //MessageBox.Show("wav File header " + riff + " " + format + " " + wavHeader.audioFormat.ToString());
+                }
+            }            
+            catch(FileNotFoundException e)
+            {
+                MessageBox.Show("cannot open the file " + e.FileName);
+            }
+
+        }
+
+        public void DrawAudio(List<short> inputValues, PictureBox picDraw)
+        {
+            Bitmap bmp;
+            int index = 0;
+            int width = picDraw.Width;
+            int height = picDraw.Height;
+            SizeF sizef=new SizeF(); RectangleF rectanglef=new RectangleF();
+            bmp = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.Black);
+                if (inputValues.Count >= 15000)
+                {
+                    List<short> inputNomalized = new List<short>();
+                    inputNomalized = waveNormalize(inputValues, width, height);
+                    for (int i = 0; i < (inputNomalized.Count) / 2 - 1; i++)
+                    {
+                        g.DrawLine(new Pen(Color.Blue), new Point(i, inputNomalized[index] + height / 2), new Point(i, inputNomalized[index + 1] + height / 2));
+                        index += 2;
+                    }
+                    
+                }
+                else
+                {
+                    PointF previous = new PointF(0, height / 2);
+                    for (int i = 0; i < inputValues.Count; i++)
+                    {
+                        float x = (float)(i * 1.0 * width / inputValues.Count);
+                        float y = (float)height / 2 + inputValues[i] * height / (1 << 16);
+                        PointF next = new PointF(x, y);
+                        g.DrawLine(new Pen(Color.Blue), previous, next);
+                        if (inputValues.Count <= 500)
+                        {
+                            sizef = new SizeF(2, 2);
+                            rectanglef = new RectangleF(previous, sizef);
+                            g.FillRectangle(new SolidBrush(Color.Gray), rectanglef);
+                        }
+                        previous = next;                      
+                    }
+                }
+                
+            }
+            picDraw.Image = bmp;
+        }
+
+        public List<Int16> waveNormalize(List<short> inputValues, int pxRangeX, int pxRangeY)
+        {
+            int dataPerPixel = inputValues.Count / pxRangeX;
+            List<short> tempValues = new List<short>();
+            List<short> outputValues = new List<short>();
+            int index = 0;
+            for (int i = 0; i + dataPerPixel <= inputValues.Count; i += dataPerPixel)
+            {
+                tempValues = inputValues.GetRange(i, dataPerPixel);
+                outputValues.Add(tempValues.Max());
+                outputValues.Add(tempValues.Min());
+                outputValues[index] = (short)((outputValues[index] * pxRangeY) / 65536);
+                outputValues[index + 1] = (short)((outputValues[index + 1] * pxRangeY) / 65536);
+                index += 2;
+            }
+            return outputValues;
+        }
+
+    }
+
+}
